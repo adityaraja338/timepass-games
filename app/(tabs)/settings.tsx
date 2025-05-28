331dab572c3f03adfc6a72844018bc965e0eb166
Sachin kumar gupta <sachinkg1309@gmail.com>
2025-05-28 23:54:34 +0530

    feat: update tab layout and add new screens for Leaderboard and Settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <Text className="text-white text-5xl font-bold">Home page</Text>
+      <Text className="text-white text-5xl font-bold">Settings page</Text>
     </View>
   );
 }
