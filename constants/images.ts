c1c08a752cfacdc03725818dd9625f6c9e9e8e02
Sachin kumar gupta <sachinkg1309@gmail.com>
2025-05-29 14:04:22 +0530

    feat: add image constants and type declarations for PNG assets

---
+++ constants/images.ts
@@ -0,0 +1,19 @@
+import tictactoe from "../assets/images/sudoko.png";
+import sudoko from "../assets/images/sudoko1.png";
+import ludo from "../assets/images/ludo.png";
+import connectFour from "../assets/images/connect-four.png";
+import checkers from "../assets/images/checkers.png";
+import minesweeper from "../assets/images/minesweeper.png";
+import image7 from "../assets/images/image7.png";
+import image8 from "../assets/images/image8.png";
+
+export const images = {
+  tictactoe,
+  sudoko,
+  ludo,
+  connectFour,
+  checkers,
+  minesweeper,
+  image7,
+  image8,
+};
